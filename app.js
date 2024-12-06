0ae988191a89ee8a4e5488ad93e2618e5c976e92
Amit Singh <amitsskingh@gmail.com>
2024-12-06 21:03:08 +0530

    update

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const app = express()
+const connectDB = require("./db/connect.js")
+require("express-async-errors")
+require("dotenv").config()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+
+const ServerError = require("./errors/server-error.js")
+const errorHandlerMiddleware = require("./middleware/error-handler.js")
+const notFoundError = require("./middleware/not-found.js")
+
+const corsOptions = {
+  origin: `${process.env.FRONTEND_URL}`, // Frontend's URL
+  credentials: true,
+}
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://textmate-frontend.netlify.app",
+// ]
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+
+app.use(cors(corsOptions))
+app.use(helmet())
+app.use(xss())
+app.use(cookieParser())
+app.use(express.json())
+
+// routes
+const bookRouter = require("./route/book.js")
+const noteRouter = require("./route/note.js")
+const authRouter = require("./route/auth.js")
+const authenticateUser = require("./middleware/authentication.js")
+
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/profile", authenticateUser, authRouter)
+app.use("/api/v1/books", authenticateUser, bookRouter)
+app.use("/api/v1/books", authenticateUser, noteRouter)
+
+app.use(notFoundError)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`)
+    })
+  } catch (error) {
+    throw new ServerError("Server error")
+    // console.log(error)
+  }
+}
+
+start()
---
+++ controller/auth.js
@@ -0,0 +1,67 @@
+const { NotFoundError, UnauthenticatedError } = require("../errors")
+const asyncWrapper = require("../middleware/async")
+const User = require("../model/User")
+
+const setCookie = (res, token) => {
+  const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+  res.cookie("token", token, {
+    expires: expires,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  })
+}
+
+const register = asyncWrapper(async (req, res) => {
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    throw new UnauthenticatedError("Password does not match")
+  }
+
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  })
+
+  const token = user.createJWT()
+  setCookie(res, token)
+  res.status(201).json({ user: { name: user.name }, token })
+})
+
+const login = asyncWrapper(async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new UnauthenticatedError("Password does not match")
+  }
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new NotFoundError(`No user with email ${email}`)
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Password does not match")
+  }
+
+  const token = user.createJWT()
+  setCookie(res, token)
+  res.status(200).json({ user: { name: user.name }, token })
+})
+
+const logout = (req, res) => {
+  res.clearCookie("token")
+  res.json({ message: "Logged out successfully" })
+}
+
+const profile = (req, res) => {
+  res.status(200).json({ username: req.user.name })
+}
+
+module.exports = {
+  register,
+  login,
+  logout,
+  profile,
+}
